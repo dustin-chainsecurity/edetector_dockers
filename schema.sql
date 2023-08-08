847e896aeb7481978849c4620246e556644b33a8
yu-niverse <chiehyu.cs09@nycu.edu.tw>
2023-08-08 10:43:06 +0800

    updated mariadb schema

--- schema.sql
+++ schema.sql
@@ -1,14 +1,13 @@
-DROP DATABASE IF EXISTS edetector;
-CREATE DATABASE edetector;
+CREATE DATABASE IF NOT EXISTS edetector;
 USE edetector;
 
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	username VARCHAR(45) NOT NULL,
 	password VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE user_info (
+CREATE TABLE IF NOT EXISTS user_info (
 	id INT NOT NULL,
 	token VARCHAR(200),
 	token_time TIMESTAMP,
@@ -17,14 +16,14 @@ CREATE TABLE user_info (
 	FOREIGN KEY (id) REFERENCES user(id) ON DELETE CASCADE
 );
 
-CREATE TABLE client(
+CREATE TABLE IF NOT EXISTS client(
   client_id varchar(45) NOT NULL,
   ip varchar(45),
   mac varchar(45),
   PRIMARY KEY (client_id)
 );
 
-CREATE TABLE client_setting(
+CREATE TABLE IF NOT EXISTS client_setting(
   client_id varchar(45),
   networkreport boolean,
   processreport boolean,
@@ -32,7 +31,7 @@ CREATE TABLE client_setting(
   FOREIGN KEY (client_id) REFERENCES client(client_id) ON DELETE CASCADE
 );
 
-CREATE TABLE client_info(
+CREATE TABLE IF NOT EXISTS client_info(
   client_id varchar(45) NOT NULL,
   sysinfo varchar(100) NOT NULL,
   osinfo varchar(100) NOT NULL,
@@ -44,7 +43,7 @@ CREATE TABLE client_info(
   FOREIGN KEY (client_id) REFERENCES client(client_id) ON DELETE CASCADE
 );
 
-CREATE TABLE group_info (
+CREATE TABLE IF NOT EXISTS group_info (
 	group_id INT AUTO_INCREMENT PRIMARY KEY,
 	group_name VARCHAR(45) NOT NULL,
 	description VARCHAR(1000) NOT NULL,
@@ -52,7 +51,7 @@ CREATE TABLE group_info (
 	range_end VARCHAR(45) NOT NULL
 );
 
-CREATE TABLE client_group (
+CREATE TABLE IF NOT EXISTS client_group (
 	client_id varchar(45) NOT NULL,
 	group_id INT NOT NULL,
 	PRIMARY KEY (client_id, group_id),
@@ -60,7 +59,7 @@ CREATE TABLE client_group (
 	FOREIGN KEY (group_id) REFERENCES group_info(group_id)
 );
 
-CREATE TABLE task (
+CREATE TABLE IF NOT EXISTS task (
 	task_id varchar(45) NOT NULL,
 	client_id varchar(45) NOT NULL,
   server_id varchar(45),
@@ -72,7 +71,7 @@ CREATE TABLE task (
 	FOREIGN KEY (client_id) REFERENCES client(client_id) ON DELETE CASCADE
 );
 
-CREATE TABLE client_task_status (
+CREATE TABLE IF NOT EXISTS client_task_status (
 	client_id varchar(45) NOT NULL,
 	scan_schedule varchar(45),
 	scan_finish_time TIMESTAMP,
@@ -84,17 +83,40 @@ CREATE TABLE client_task_status (
 	FOREIGN KEY (client_id) REFERENCES client(client_id) ON DELETE CASCADE
 );
 
-INSERT INTO user (username, password) VALUES ('test', MD5('123'));
-INSERT INTO user_info (id, token, email) VALUES ('1', '123', 'test@example.com');
+INSERT INTO user (username, password)
+SELECT 'test', MD5('123')
+WHERE NOT EXISTS (
+    SELECT 1 FROM user WHERE username = 'test'
+);
 
-INSERT INTO user (username, password) VALUES ('chiehyu', MD5('123'));
-INSERT INTO user_info (id, email) VALUES ('2', 'chiehyu@example.com');
+INSERT INTO user_info (id, token, email)
+SELECT '1', '123', 'test@example.com'
+WHERE NOT EXISTS (
+    SELECT 1 FROM user_info WHERE id = '1'
+);
 
-INSERT INTO user (username, password) VALUES ('beta', MD5('123'));
-INSERT INTO user_info (id, email) VALUES ('3', 'beta@example.com');
+INSERT INTO user (username, password)
+SELECT 'chiehyu', MD5('123')
+WHERE NOT EXISTS (SELECT 1 FROM user WHERE username = 'chiehyu')
+UNION
+SELECT 'beta', MD5('123')
+WHERE NOT EXISTS (SELECT 1 FROM user WHERE username = 'beta')
+UNION
+SELECT 'bob', MD5('456')
+WHERE NOT EXISTS (SELECT 1 FROM user WHERE username = 'bob')
+UNION
+SELECT 'QA', MD5('123')
+WHERE NOT EXISTS (SELECT 1 FROM user WHERE username = 'QA');
 
-INSERT INTO user (username, password) VALUES ('bob', MD5('456'));
-INSERT INTO user_info (id, email) VALUES ('4', 'bob@example.com');
-
-INSERT INTO user (username, password) VALUES ('QA', MD5('123'));
-INSERT INTO user_info (id, email) VALUES ('5', 'qa@example.com');
+INSERT INTO user_info (id, email)
+SELECT '2', 'chiehyu@example.com'
+WHERE NOT EXISTS (SELECT 1 FROM user_info WHERE id = '2')
+UNION
+SELECT '3', 'beta@example.com'
+WHERE NOT EXISTS (SELECT 1 FROM user_info WHERE id = '3')
+UNION
+SELECT '4', 'bob@example.com'
+WHERE NOT EXISTS (SELECT 1 FROM user_info WHERE id = '4')
+UNION
+SELECT '5', 'qa@example.com'
+WHERE NOT EXISTS (SELECT 1 FROM user_info WHERE id = '5');
